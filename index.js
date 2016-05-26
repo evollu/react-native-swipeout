aa51ed7caece125447cbcf3459a6e6e0a8bf0305
Libin Lu <llu@evolenthealth.com>
2016-05-26 11:22:21 -0400

    break react and react native

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const React = require('react-native');
+const React = require('react');
 const {
   Animated,
   PanResponder,
   StyleSheet,
   View
-} = React
+} = require('react-native')
 
 const Btn = require('./components/btn.js');
 
